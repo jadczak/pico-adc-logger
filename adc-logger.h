bddf881b313ba80e4e594e732c93b245f9bd66ad
Alexander Jadczak <jadczak@gmail.com>
2025-03-02 20:31:41 -0500

    ran clang-format

--- adc-logger.h
+++ adc-logger.h
@@ -3,5 +3,5 @@
 
 absolute_time_t debug_get_time(void);
 int64_t debug_time_diff(absolute_time_t start, absolute_time_t end);
-void debug_uart_print(uart_inst_t *uart, const char *str);
-bool sample_adc_callback(struct repeating_timer *t);
+void debug_uart_print(uart_inst_t* uart, const char* str);
+bool sample_adc_callback(struct repeating_timer* t);
